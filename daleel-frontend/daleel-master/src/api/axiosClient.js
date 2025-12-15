1c7a7c37903b2b073f97b2f28ff6bb9beb9eb415
Saif Amir <95121206+SaifAmirr@users.noreply.github.com>
2025-12-15 17:26:16 +0200

    Set base URL for axios client

--- daleel-frontend/daleel-master/src/api/axiosClient.js
+++ daleel-frontend/daleel-master/src/api/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
+  baseURL: 'http://daleel-test-for-deployment.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
